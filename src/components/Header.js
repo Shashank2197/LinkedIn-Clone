8a8b28bc114a82d59117622c459e4c0ef4eb6965
Shashank G Acharya <shashuviper21@gmail.com>
2022-09-02 15:12:41 +0530

    header css changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,25 @@
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
 
 const Header = () => {
   return (
     <>
-      <div className="headers">
-        <h1>This is the headers</h1>
+      <div className="header">
         <div className="header__left">
-          <LinkedInIcon color="primary" fontSize="large" />
+          <LinkedInIcon color="primary" fontSize="large" className="image" />
           <div className="header__search">
             <SearchIcon />
             <input type="text" />
           </div>
         </div>
-        <div className="header__right"></div>
+        <div className="header__right">
+          <HeaderOption Icon={HomeIcon} title="Home" />
+          <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        </div>
       </div>
     </>
   );
